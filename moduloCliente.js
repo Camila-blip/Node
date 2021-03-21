8a1cbe4a61594e91fd7224f0e74ab4f19bed93e2
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2021-03-21 17:26:44 -0300

    Add files via upload
    Chamando a exportação

---
+++ moduloCliente.js
@@ -0,0 +1,8 @@
+const moduloA = require('./modulo')
+const moduloB = require('./modulob')
+// se estiver na mesma pagina é necessario ./ se for pastas atras ../
+console.log(moduloA.ola)
+console.log(moduloB.bomDia)
+ 
+// npm i lodash
+//npm i -g nodemon
